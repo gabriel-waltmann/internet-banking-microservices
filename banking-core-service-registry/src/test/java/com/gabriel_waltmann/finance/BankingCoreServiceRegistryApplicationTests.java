80c251bfbb3c7fc7dbd7dc4a8e4df4ccbc579b9d
gabriel-waltmann <gabrielwaltmann@gmail.com>
2025-09-13 11:35:54 -0300

    feat: add banking-core-service-registry

---
+++ banking-core-service-registry/src/test/java/com/gabriel_waltmann/finance/BankingCoreServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabriel_waltmann.finance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankingCoreServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
